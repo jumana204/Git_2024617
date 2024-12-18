321b8e47a7da56c8d7341570946a9c190f73f7c6
Jumana <2024617@student.cct.ie>
2024-12-18 21:36:37 +0000

    Initial commit Added a simple print

--- src/git_2024617/Git_2024617.java
+++ src/git_2024617/Git_2024617.java
@@ -15,6 +15,7 @@ public class Git_2024617 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Jumana-2024617");
     }
     
 }
